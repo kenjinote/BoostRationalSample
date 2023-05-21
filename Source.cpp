166b9cd07917d208abcb52a56e60f2b0f0faedf9
kenjinote <2605401+kenjinote@users.noreply.github.com>
2023-05-21 12:15:46 +0900

    プロジェクト ファイルを追加します。

---
+++ Source.cpp
@@ -0,0 +1,99 @@
+﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include <windows.h>
+
+#include <boost/rational.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/regex.hpp>
+
+using namespace boost::multiprecision;
+
+TCHAR szClassName[] = TEXT("Window");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static HWND hButton;
+	static HWND hEdit1;
+	static HWND hEdit2;
+	switch (msg)
+	{
+	case WM_CREATE:
+		hEdit1 = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), L"Hello World!", WS_VISIBLE | WS_CHILD | ES_AUTOHSCROLL, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hButton = CreateWindow(TEXT("BUTTON"), TEXT("有理数型に変換"), WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, hWnd, (HMENU)IDOK, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hEdit2 = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), 0, WS_VISIBLE | WS_CHILD | ES_AUTOHSCROLL, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		break;
+	case WM_SIZE:
+		MoveWindow(hEdit1, 10, 10, 256, 32, TRUE);
+		MoveWindow(hButton, 10, 50, 256, 32, TRUE);
+		MoveWindow(hEdit2, 10, 90, 256, 32, TRUE);
+		break;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK) {
+			CHAR szText[1024];
+			GetWindowTextA(hEdit1,szText, _countof(szText));
+			std::string s = szText;
+			if (regex_match(s, boost::regex("^[+-]?[0-9]+(\\.([0-9]+)?)?$"))) {
+				boost::rational<cpp_int> r;
+				size_t index = s.find(".");
+				if (index == std::string::npos) {
+					r = boost::rational<cpp_int>(boost::lexical_cast<cpp_int>(s), 1);
+				}
+				else {
+					size_t len = s.length() - index - 1;
+					cpp_int denominator = pow(cpp_int(10), (const unsigned int)len);
+					cpp_int numerator = boost::lexical_cast<cpp_int>(s.substr(0, index) + s.substr(index + 1, len));
+					r = boost::rational<cpp_int>(numerator, denominator);
+				}
+				cpp_dec_float_100 d = boost::rational_cast<cpp_dec_float_100>(r);
+				sprintf_s(szText, "%s / %s = %s", r.numerator().str().c_str(), r.denominator().str().c_str(), d.str().c_str());
+				SetWindowTextA(hEdit2, szText);
+			}
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+{
+	MSG msg;
+	WNDCLASS wndclass = {
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0,IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		TEXT("Window"),
+		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+	);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while (GetMessage(&msg, 0, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
